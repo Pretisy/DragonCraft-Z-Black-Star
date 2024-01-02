56762770916a4c41fde385cb4b940d9161301b05
Pretisy <Pretisy@gmail.com>
2024-01-02 17:24:59 -0500

    Added Core Files.
    Added the unedited core files that would allow us to create the Mod with Fabric.

---
+++ src/main/java/net/pretisy/dragoncraft/z/black/star/DragonCraftZBlackStarDataGenerator.java
@@ -0,0 +1,11 @@
+package net.pretisy.dragoncraft.z.black.star;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DragonCraftZBlackStarDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
