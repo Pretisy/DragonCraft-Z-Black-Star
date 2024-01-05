6b43f4f554055d9fb6e88509f617ade37b8f7a7c
Pretisy <Pretisy@gmail.com>
2024-01-05 17:48:23 -0500

    Added Senzu Beans.
    We added Minor & Greater versions of Senzu Beans as in game items.

---
+++ src/main/java/net/pretisy/dragoncraftzblackstar/DragonCraftZBlackStarClient.java
@@ -0,0 +1,10 @@
+package net.pretisy.dragoncraftzblackstar;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DragonCraftZBlackStarClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
