27caf398d49e60a6c480f8987159dcd99d6a9346
Pretisy <Pretisy@gmail.com>
2024-01-06 14:48:28 -0500

    Updated Core Files.
    The Core Files were broken. So we had to start from scratch.

--- src/main/java/net/pretisy/dragoncraftzblackstar/DragonCraftZBlackStar.java
+++ src/main/java/net/pretisy/dragoncraftzblackstar/DragonCraftZBlackStar.java
@@ -2,17 +2,16 @@ package net.pretisy.dragoncraftzblackstar;
 
 import net.fabricmc.api.ModInitializer;
 
-import net.pretisy.dragoncraftzblackstar.items.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DragonCraftZBlackStar implements ModInitializer {
-	public static String MOD_ID = "dragoncraftzblackstar";
-    public static final Logger LOGGER = LoggerFactory.getLogger("MOD_ID");
+	public static final String MOD_ID = "dragoncraftzblackstar";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
 
-		ModItems.registerModItems();
+		LOGGER.info("Hello Fabric world!");
 	}
 }
--- src/main/java/net/pretisy/dragoncraftzblackstar/DragonCraftZBlackStarClient.java
+++ src/main/java/net/pretisy/dragoncraftzblackstar/DragonCraftZBlackStarClient.java
@@ -2,7 +2,7 @@ package net.pretisy.dragoncraftzblackstar;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class DragonCraftZBlackStarClient implements ClientModInitializer{
+public class DragonCraftZBlackStarClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
 
