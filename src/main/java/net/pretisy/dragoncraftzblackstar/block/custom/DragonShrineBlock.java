d108a4b900e8f7e4dffcb5fcd45799b62b6602f1
Pretisy <Pretisy@gmail.com>
2024-01-26 11:36:17 -0500

    Added ToolTips To The Dragon Radar Item, & Dragon Shrine Block.
    Just to help explain what those two do in game.

--- src/main/java/net/pretisy/dragoncraftzblackstar/block/custom/DragonShrineBlock.java
+++ src/main/java/net/pretisy/dragoncraftzblackstar/block/custom/DragonShrineBlock.java
@@ -2,14 +2,21 @@ package net.pretisy.dragoncraftzblackstar.block.custom;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class DragonShrineBlock extends Block {
     public DragonShrineBlock(Settings settings) {
@@ -21,5 +28,11 @@ public class DragonShrineBlock extends Block {
                               PlayerEntity player, Hand hand, BlockHitResult hit) {
         world.playSound(player, pos, SoundEvents.BLOCK_AMETHYST_BLOCK_CHIME, SoundCategory.BLOCKS, 1f, .5f);
         return ActionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable BlockView world, List<Text> tooltip, TooltipContext options) {
+        tooltip.add(Text.translatable("tooltip.dragoncraftzblackstar.dragon_shrine.tooltip"));
+        super.appendTooltip(stack, world, tooltip, options);
     }
 }
--- src/main/java/net/pretisy/dragoncraftzblackstar/item/custom/DragonRadarItem.java
+++ src/main/java/net/pretisy/dragoncraftzblackstar/item/custom/DragonRadarItem.java
@@ -3,13 +3,16 @@ package net.pretisy.dragoncraftzblackstar.item.custom;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -78,5 +81,11 @@ public class DragonRadarItem extends Item {
                 //state.isOf(ModBlocks.BLACK_STAR_DRAGON_BALL) ||
                 //state.isOf(ModBlocks.SUPER_DRAGON_BALL) ||
                 //state.isOf(ModBlocks.OMNIFICENCE_CRYSTAL);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(Text.translatable("tooltip.dragoncraftzblackstar.dragon_radar.tooltip"));
+        super.appendTooltip(stack, world, tooltip, context);
     }
 }
