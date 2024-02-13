bb4208ad872b638622807cd508fcc0bb0b759549
Pretisy <Pretisy@gmail.com>
2024-02-13 16:50:10 -0500

    Rebalanced All Consumables V.2.
    After some testing, we felt like it wasn't enough hunger & saturation.
    So hopefully these feel better.

--- src/main/java/net/pretisy/dragoncraftzblackstar/item/ModFoodComponents.java
+++ src/main/java/net/pretisy/dragoncraftzblackstar/item/ModFoodComponents.java
@@ -6,8 +6,8 @@ import net.minecraft.item.FoodComponent;
 
 public class ModFoodComponents {
     public static final FoodComponent LESSER_SENZU_BEAN = new FoodComponent.Builder()
-            .hunger(2)
-            .saturationModifier(2)
+            .hunger(10)
+            .saturationModifier(10)
             .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 200, 1), 1.0F)
             .statusEffect(new StatusEffectInstance(StatusEffects.INSTANT_HEALTH, 1, 1), 1.0F)
             .alwaysEdible()
@@ -15,8 +15,8 @@ public class ModFoodComponents {
             .build();
 
     public static final FoodComponent GREATER_SENZU_BEAN = new FoodComponent.Builder()
-            .hunger(10)
-            .saturationModifier(10)
+            .hunger(20)
+            .saturationModifier(20)
             .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 300, 5), 1.0F)
             .statusEffect(new StatusEffectInstance(StatusEffects.INSTANT_HEALTH, 1, 10), 1.0F)
             .statusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 20, 2), 1.0F)
@@ -25,8 +25,8 @@ public class ModFoodComponents {
             .build();
 
     public static final FoodComponent DARK_SENZU_BEAN = new FoodComponent.Builder()
-            .hunger(2)
-            .saturationModifier(2)
+            .hunger(10)
+            .saturationModifier(10)
             .statusEffect(new StatusEffectInstance(StatusEffects.POISON, 200, 4), 1.0F)
             .statusEffect(new StatusEffectInstance(StatusEffects.INSTANT_DAMAGE, 1, 5), 1.0F)
             .alwaysEdible()
@@ -34,8 +34,8 @@ public class ModFoodComponents {
             .build();
 
     public static final FoodComponent ULTRA_DIVINE_WATER = new FoodComponent.Builder()
-            .hunger(0)
-            .saturationModifier(0)
+            .hunger(-10)
+            .saturationModifier(-10)
             .statusEffect(new StatusEffectInstance(StatusEffects.POISON, 100), 1.0F)
             .statusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 400, 5), 1.0F)
             .statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 400, 5), 1.0F)
@@ -45,8 +45,8 @@ public class ModFoodComponents {
             .build();
 
     public static final FoodComponent A_LA_PLANETAISE = new FoodComponent.Builder()
-            .hunger(6)
-            .saturationModifier(6)
+            .hunger(12)
+            .saturationModifier(12)
             .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 400, 2), 1.0F)
             .statusEffect(new StatusEffectInstance(StatusEffects.LUCK, 400, 5), 1.0F)
             .alwaysEdible()
@@ -54,22 +54,22 @@ public class ModFoodComponents {
             .build();
 
     public static final FoodComponent CANDY = new FoodComponent.Builder()
-            .hunger(2)
-            .saturationModifier(3)
+            .hunger(4)
+            .saturationModifier(6)
             .statusEffect(new StatusEffectInstance(StatusEffects.INSTANT_HEALTH, 1, 0), 1.0F)
             .alwaysEdible()
             .snack()
             .build();
 
     public static final FoodComponent PUDDING = new FoodComponent.Builder()
-            .hunger(4)
-            .saturationModifier(6)
+            .hunger(8)
+            .saturationModifier(8)
             .snack()
             .build();
 
     public static final FoodComponent CHOCOLATE = new FoodComponent.Builder()
-            .hunger(2)
-            .saturationModifier(2)
+            .hunger(3)
+            .saturationModifier(3)
             .snack()
             .build();
 
